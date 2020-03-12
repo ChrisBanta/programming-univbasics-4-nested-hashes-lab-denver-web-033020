8772b159ade538bccd72bc9b421d1d2f6c4e1c2e
Chris Banta <chrisbanta1@gmail.com>
2020-03-12 20:11:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,8 +134,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-		programmer_hash[:dennis_ritchie] = {
-	:known_for => "GUI"
-}
+		programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 	programmer_hash
 end
